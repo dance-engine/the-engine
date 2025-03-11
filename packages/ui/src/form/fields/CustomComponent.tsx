e57ee9ef6fd62fd2980f95b4684804e416e0338f
Adam Bardsley <adam.bardsley@gmail.com>
2025-03-11 14:39:31 +0000

    Removing debug

--- packages/ui/src/form/fields/CustomComponent.tsx
+++ packages/ui/src/form/fields/CustomComponent.tsx
@@ -12,12 +12,12 @@ interface CustomComponentProps {
 
 const CustomComponent: React.FC<CustomComponentProps> = ({ label, error, fieldSchema, children }) => {
   const description = fieldSchema._def.description ?? "";
-  const typeName = fieldSchema._def.typeName; // For debugging
+  // const typeName = fieldSchema._def.typeName; // For debugging
 
   return (
     <div className="flex flex-col">
       <label className="font-semibold capitalize">
-        {label} <span className="text-gray-400 text-xs">({typeName})</span>
+        {label} <span className="text-gray-400 text-xs">{/*({typeName})*/}</span>
       </label>
       {children}
       {error ? <p className="text-red-500 text-sm">{error}</p> : description && <p className="text-gray-500 text-sm">{description}</p>}

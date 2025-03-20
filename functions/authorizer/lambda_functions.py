9d4968169c2bd229f7d99815730bd6432fcc4df9
Adam Bardsley <adam.bardsley@gmail.com>
2025-03-20 10:00:36 +0000

    🔐🧱 Moving API auth/usage to ENV from SSM

--- functions/authorizer/lambda_functions.py
+++ functions/authorizer/lambda_functions.py
@@ -1,26 +1,17 @@
+import os
 import jwt
 import logging
 
 logger = logging.getLogger()
 logger.setLevel("INFO")
 
-PUBLIC_KEY = """-----BEGIN PUBLIC KEY-----
-MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAtnBAijofwYzYAfLqF/Zb
-aaBqzdoVVi15M6xhnyyT8eRej0lPn0jw+S0DKzGzf61YqZBjLzcFNRJwwWEXwPtU
-iwKFvJqgoovyIodJxjnqp3r1ZqTbJSYgzZTebLYuhGlKXdUX+vsiW4Q9wrlcAD7q
-t0OSBLfzQ8crdFkW/9VcTpbdG7K2HxKxuBSMP5a5ZuU9QMAuWiP6KSgsbQFCs5RQ
-wzredqJj3SiRDrY5L1RSNL9vsZov1yzCWmcysOx78ITobpWtEdP52x3jcaXkeCez
-9beH+tKx8jBI0ly1tmzxbrm5JkJaaECgnnPNvT9MLFIUnfcdPKI8xRKM6Nqqdo6X
-YwIDAQAB
------END PUBLIC KEY-----
-"""
+PUBLIC_KEY = os.environ.get("CLERK_KEY")
 
 reserved_values = ["admin", "superuser", "internal","danceengine","dance-engine","public"]
 
 def auth_handler(event, context):
     logger.info(f"{event},{context}")
 
-    #TODO Change customer to organisation
     organisation = event.get("pathParameters", {}).get("organisation")
     authorization_header = event.get("headers", {}).get("authorization")
     token = authorization_header.split(' ')[1]

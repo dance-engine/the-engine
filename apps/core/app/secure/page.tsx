c80a23b7cb0b4cccf177ffe72913a19e97ebc810
Adam Bardsley <adam.bardsley@gmail.com>
2025-05-13 15:16:59 +0100

    👽️ update to work with current API structure

--- apps/core/app/secure/page.tsx
+++ apps/core/app/secure/page.tsx
@@ -4,7 +4,7 @@ import useClerkSWR from '@dance-engine/utils/clerkSWR';
 import { useOrgContext } from '@dance-engine/utils/OrgContext';
 
 // const mealSummaryApiUrl = "https://a5qvybvrc7.execute-api.eu-west-1.amazonaws.com/test"
-const exampleApiCall = `${process.env.NEXT_PUBLIC_DANCE_ENGINE_API}/{org}/events`
+const exampleApiCall = `${process.env.NEXT_PUBLIC_DANCE_ENGINE_API}{org}/events`
 
 export default function SecurePage() {
   // const {data: summaryData, error: summaryError, isLoading: summaryLoading, isValidating: summaryValidating} = useClerkSWR(exampleApiCall);
@@ -15,6 +15,6 @@ export default function SecurePage() {
   }
   return <div>
     <h1 className='text-xl'>Secured {activeOrg}</h1>
-    <pre>{JSON.stringify([...summaryData.map((r: EventType) => { return {...r, description: JSON.parse(r.description)} })],null,2)}</pre>
+    <pre>{summaryData && JSON.stringify([...summaryData.events.map((r: EventType) => { return {...r, description: JSON.parse(r.description)} })],null,2)}</pre>
   </div>
 }

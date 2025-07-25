3d3cbf829b5a4689090c63931fe29ec9ff973b19
Adam Bardsley <adam.bardsley@gmail.com>
2025-07-25 10:15:32 +0100

    🔧 Updates organization data mapping in profile page
    Refines data handling by specifically accessing
    organization sites from `summaryData.organisations`.
    
    Enhances code reliability and clarity in data structure management.

--- apps/core/app/user/profile/[[...profile...]]/page.tsx
+++ apps/core/app/user/profile/[[...profile...]]/page.tsx
@@ -61,7 +61,7 @@ const CustomPage = () => {
       { sitesAdmind.includes("*") ? (
         <div>
           
-          {summaryData && summaryData.map((site: Record<string, string>)=>{
+          {summaryData && summaryData.organisations && summaryData.organisations.map((site: Record<string, string>)=>{
 
             const orgSlug = site["org-slug"] || ""
             const active = orgSlug == activeOrg

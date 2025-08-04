7b3c5ca235b1792eb5af20b54f73f183db6c886a
Connor Monaghan <connor1monaghan@gmail.com>
2025-08-05 01:15:52 +0200

    🚧 Implements create bundles

--- functions/functions/bundles/lambda_bundles.py
+++ functions/functions/bundles/lambda_bundles.py
@@ -10,7 +10,7 @@ from datetime import datetime, timezone
 ## installed packages
 from pydantic import AfterValidator, ValidationError # layer: pydantic
 #from boto3.dynamodb.conditions import Key
-#from ksuid import KsuidMs # layer: utils
+from ksuid import KsuidMs # layer: utils
 
 ## custom scripts
 sys.path.append(os.path.dirname(__file__))
@@ -21,18 +21,19 @@ from _pydantic.models.bundles_models import BundleObject, BundleResponse, Create
 from _pydantic.models.models_extended import BundleModel
 #from _pydantic.EventBridge import triggerEBEvent, trigger_eventbridge_event, EventType, Action # pydantic layer
 #from _pydantic.dynamodb import VersionConflictError # pydantic layer
-#from _pydantic.dynamodb import batch_write, transact_upsert, VersionConflictError # pydantic layer
+from _pydantic.dynamodb import batch_write, transact_upsert, VersionConflictError # pydantic layer
 
 ## logger setup
 logger = logging.getLogger()
 logger.setLevel("INFO")
 
-## aws resources an clients
-#db = boto3.resource("dynamodb")
+db = boto3.resource("dynamodb")
+eventbridge = boto3.client('events')
 
 ## ENV variables
 # will throw an error if the env variable does not exist
 STAGE_NAME = os.environ.get('STAGE_NAME') or (_ for _ in ()).throw(KeyError("Environment variable 'STAGE_NAME' not found"))
+ORG_TABLE_NAME_TEMPLATE = os.environ.get('ORG_TABLE_NAME_TEMPLATE') or (_ for _ in ()).throw(KeyError("Environment variable 'ORG_TABLE_NAME_TEMPLATE' not found"))
 
 def get_one(organisationSlug: str,  eventId: str,  bundleId: str, public: bool = False, actor: str = "unknown"):
     '''
@@ -51,20 +52,75 @@ def get_all(organisationSlug: str,  eventId: str, public: bool = False, actor: s
     # TODO: implement
     return make_response(201, {
             "message": "It's a work in progress... You expect me to return a list of instances of BundleObject.",
-            "items": [{"message": "You expect me to return a list of instances of BundleObject."}]
+            "bundles": [{"message": "You expect me to return a list of instances of BundleObject."}]
         })
 
 def update(request: UpdateBundleRequest, organisationSlug: str, eventId: str, actor: str = "unknown"):
     return make_response(201, {
             "message": "It's a work in progress...",
-            "items": [{"message": "You expect me to return a list of instances of BundleObject I just updated."}]
+            "bundles": [{"message": "You expect me to return a list of instances of BundleObject I just updated."}]
         })
 
 def create(request: CreateBundleRequest, organisationSlug: str, eventId: str, actor: str = "unknown"):
-    return make_response(201, {
-            "message": "It's a work in progress...",
-            "items": [{"message": "You expect me to return a list of instances of BundleObject I just created."}]
-        })
+    logger.info(f"Create Bundle(s): {request}")
+
+    TABLE_NAME = ORG_TABLE_NAME_TEMPLATE.replace("org_name",organisationSlug)
+    table = db.Table(TABLE_NAME)
+    logger.info(f"Adding bundle(s) for {eventId} of {organisationSlug} into {TABLE_NAME}")
+
+    current_time = datetime.now(timezone.utc).isoformat(timespec='milliseconds').replace('+00:00', 'Z')
+    logger.info(f"Current Time: {current_time}")
+
+    bundles_data  = request.bundles
+    bundles_models = []
+    for bd in bundles_data:
+        bundles_models.append(BundleModel.model_validate({
+            **bd.model_dump(mode="json", exclude_unset=True),
+            "ksuid": str(bd.ksuid) if bd.ksuid else str(KsuidMs()),
+            "organisation": organisationSlug,
+            "parent_event_ksuid": eventId,
+            "created_at": current_time,
+            "updated_at": current_time
+        }))
+
+    # validate that referenced items in includes exist
+    item_ids = {item_id for bm in bundles_models if bm.includes for item_id in bm.includes}
+    if item_ids:
+        client = table.meta.client
+        request_keys = [
+            {'PK': f'ITEM#{item_id}', 'SK': f'EVENT#{eventId}'}
+            for item_id in item_ids
+        ]
+        resp = client.batch_get_item(RequestItems={TABLE_NAME: {'Keys': request_keys}})
+        found_items = resp.get('Responses', {}).get(TABLE_NAME, [])
+        found_ids = {item['PK'].split('#', 1)[1] for item in found_items}
+        missing = list(item_ids - found_ids)
+        if missing:
+            logger.error(f"Cannot create bundle(s): missing item(s): {missing}")
+            return make_response(400, {
+                'message': 'Cannot create bundle(s): referenced item(s) not found',
+                'missing_items': missing
+            })
+
+    try:
+        successful_bundles, unprocessed_bundles = batch_write(table, bundles_models)
+
+        if len(unprocessed_bundles) > 0:
+            logger.warning(f"Unprocessed bundles: {unprocessed_bundles}")
+            return make_response(207, {
+                "message": "Bundles created with some unprocessed bundles.",
+                "bundles": [bundle.model_dump_json() for bundle in successful_bundles],
+                "unprocessed": [bundle.model_dump_json() for bundle in unprocessed_bundles]
+            })
+        else:
+            return make_response(201, {
+                "message": "Bundles created successfully.",
+                "bundles": [bundle.model_dump_json() for bundle in successful_bundles],
+            })
+    except Exception as e:
+        logger.error("Unexpected error: %s", str(e))
+        logger.error(traceback.format_exc())
+        return make_response(500, {"message": "Something went wrong."})
 
 def lambda_handler(event, context):
     try:
@@ -101,7 +157,7 @@ def lambda_handler(event, context):
             else:
                 return get_all(organisationSlug, eventId, public=is_public)
                 result = get_all(organisationSlug, eventId, public=is_public)
-                response = list_response_cls(items=result)
+                response = list_response_cls(bundles=result)
             
             return make_response(200, response.model_dump(mode="json", exclude_none=True))
 

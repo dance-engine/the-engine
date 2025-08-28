cd93941ff0c48e05a276e3443b67d128773e64dc
Adam Bardsley <adam.bardsley@gmail.com>
2025-08-28 14:57:55 +0100

    🔧 Updates header layout for improved accessibility
    Modifies header component to make logo and name responsive. Ensures layout is centered with proper styling for mobile and desktop views.

--- apps/clients/solo/components/header/Header.tsx
+++ apps/clients/solo/components/header/Header.tsx
@@ -6,8 +6,8 @@ import { OrganisationType } from "@dance-engine/schemas/organisation"
 const Header = ({org}:{org: OrganisationType}) => {
   return (
     <header className='w-full bg-black text-white flex justify-center'>
-      <div className='max-w-4xl w-4xl px-4 uppercase font-black lg:px-0 py-3 flex items-center justify-center'>
-        <Link href="/" className='block'>{org.logo ? <img src={org.logo} alt={org.name} className='max-w-48 w-48'/> : org.name}</Link>
+      <div className='max-w-4xl w-4xl px-4 uppercase font-black lg:px-0 py-3 '>
+        <Link href="/" className='block w-full flex items-center justify-center'>{org.logo ? <img src={org.logo} alt={org.name} className='max-w-full w-2/3 sm:w-48'/> : org.name}</Link>
       </div>
     </header>
   )

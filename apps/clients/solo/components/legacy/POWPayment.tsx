37c0794d31fef8e95c35c4c9989f9269158b0de9
Adam Bardsley <adam.bardsley@gmail.com>
2025-11-06 10:52:43 +0000

    🛠️ Updates cart value for early bird ticket
    Modifies the cart value for the early bird ticket to £65.
    Removes the displayed text about discounted tickets for clarity.

--- apps/clients/solo/components/legacy/POWPayment.tsx
+++ apps/clients/solo/components/legacy/POWPayment.tsx
@@ -7,13 +7,13 @@ export default function POWPayment({org}: {org: {account_id: string}}) {
     <div className='w-full px-4 lg:px-0 flex justify-center border-t-6' style={{backgroundColor: 'var(--main-bg-color)', borderColor: 'var(--highlight-color)'}}>
            <div className={`max-w-4xl px-4 lg:px-0 py-24 flex flex-col items-center\ `}>
             <h2 className='text-4xl font-bold mb-4'>Early Bird Ticket</h2>
-            <p className='mb-6 text-xl'>We have a limited amount of early bird discounted tickets at only £40</p>
+            {/* <p className='mb-6 text-xl'>We have a limited amount of early bird discounted tickets at only £40</p> */}
             <StripePurchaseButton 
               accountId={org.account_id || 'acct_1Rkp1ODIMY9TzhzF'} //! Work out why accountID is missing
               // couponCode={ inProd ? "fVKhBZim" : 'u0trAdPd' } // 🔥 Live : 🔨 Test coupon code
               priceId={ inProd ? "price_1RkrE1DIMY9TzhzF2AFDc6q3" : 'price_1RnirUDIMY9TzhzFCSo3uo6K' } // 🔥 Live : 🔨 Test price ID
               style={{backgroundColor: 'var(--highlight-color)'}} className='rounded px-8 py-6 text-4xl'  
-              cartValue={4000} 
+              cartValue={6500} 
             />
            </div>
         </div>

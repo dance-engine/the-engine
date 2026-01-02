63112b37ef6b301110ed0a9c794d4c8ca6db7d25
Adam Bardsley <adam.bardsley@gmail.com>
2026-01-02 11:51:48 +0000

    ✨ Enhances hero section with overlay support
    Improves the hero section styling for specific organizations
    by adding an optional overlay image to enhance visual appeal.

--- apps/clients/solo/app/page.tsx
+++ apps/clients/solo/app/page.tsx
@@ -75,8 +75,12 @@ export default async function IndexPage() {
       <main className='w-full justify-center' >
 
         { orgSlug == 'rebel-sbk' ? 
-        <div id="hero" className='w-full bg-contain bg-no-repeat bg-center flex items-center justify-center text-white font-bold bg-black' style={{ backgroundImage: `url(${org.banner})` }}>
+        <div id="hero" className='w-full bg-contain bg-no-repeat relative bg-center flex items-center justify-center text-white font-bold bg-black' style={{ backgroundImage: `url(${org.banner})` }}>
           <img src={org.banner} alt="" className='' />
+          { org.banner_overlay && <div className='absolute bottom-0 right-0 left-0 border-green-500  overflow-hidden h-[100%] flex flex-col items-end justify-end xl:items-center' >
+            {/* <h1 className='text-4xl text-center px-6 font-heading-latin-soul'>&lsquo;Liverpool Moves to Latin Soul.&rsquo;</h1> */}
+            <img src={org.banner_overlay} alt="Banner Overlay" className='object-top object-contain block max-h-[600px] w-[200px] sm:w-[300px] md:w-[400px] lg:w-[800px] ' />
+          </div>}
         </div> : 
         <div id="hero" className={`w-full h-[60vh] XXmin-h-[550px] max-h-[1024px] 
         text-white flex items-stretch justify-center bg-no-repeat \

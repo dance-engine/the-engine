315d7a685b155bd7c570022f6abefc527f556e89
Connor Monaghan <connor1monaghan@gmail.com>
2026-02-19 00:57:26 +0000

    🚧 Start Ticket creation from eventbridge
    Work in progress

--- functions/functions/tickets/lambda_tickets.py
+++ functions/functions/tickets/lambda_tickets.py
@@ -9,74 +9,75 @@ from datetime import datetime, timezone
 
 ## installed packages
 from pydantic import AfterValidator, ValidationError # layer: pydantic
-#from boto3.dynamodb.conditions import Key
-#from ksuid import KsuidMs # layer: utils
+# from boto3.dynamodb.conditions import Key
+from ksuid import KsuidMs # layer: utils
 
 ## custom scripts
 sys.path.append(os.path.dirname(__file__))
 from _shared.parser import parse_event
 from _shared.DecimalEncoder import DecimalEncoder
 from _shared.helpers import make_response
-# from _pydantic.models.tickets_models import TicketsObject, TicketsResponse
-#from _pydantic.models.models_extended import TicketsModel
-#from _pydantic.EventBridge import triggerEBEvent, trigger_eventbridge_event, EventType, Action # pydantic layer
+from _pydantic.models.models_extended import TicketModel, TicketChildModel
+from _pydantic.EventBridge import trigger_eventbridge_event, EventType, Action, EventBridgeEvent # pydantic layer
 #from _pydantic.dynamodb import VersionConflictError # pydantic layer
-#from _pydantic.dynamodb import batch_write, transact_upsert, VersionConflictError # pydantic layer
+from _pydantic.dynamodb import batch_write, transact_upsert, VersionConflictError # pydantic layer
 
 ## logger setup
 logger = logging.getLogger()
 logger.setLevel("INFO")
 
 ## aws resources an clients
-#db = boto3.resource("dynamodb")
+db = boto3.resource("dynamodb")
 
 ## ENV variables
 # will throw an error if the env variable does not exist
 STAGE_NAME = os.environ.get('STAGE_NAME') or (_ for _ in ()).throw(KeyError("Environment variable 'STAGE_NAME' not found"))
+ORG_TABLE_NAME_TEMPLATE = os.environ.get('ORG_TABLE_NAME_TEMPLATE') or (_ for _ in ()).throw(KeyError("Environment variable 'ORG_TABLE_NAME_TEMPLATE' not found"))
 
-## write here the code which is called from the handler
-def get(organisationSlug: str, public: bool = False, actor: str = "unknown") -> TicketsObject:
-    '''
-    You expect me to return an instance of TicketsObject.
-    '''
-    # TODO: implement
-    return make_response(201, {
-            "message": "It's a work in progress...",
-        })
+from _pydantic.EventBridge import EventBridgeEventDetail  # Ensure this import is present
+
+def create_ticket(request_data: EventBridgeEvent, organisation_slug: str, actor: str = "unknown"):
+    logger.info(f"Creating ticket: {request_data}")
+    event_detail: EventBridgeEventDetail = request_data.detail if "detail" in request_data else {}
+    meta = event_detail.meta if "meta" in event_detail else {}
+
+    TABLE_NAME = ORG_TABLE_NAME_TEMPLATE.replace("org_name", organisation_slug)
+    table = db.Table(TABLE_NAME)
+    logger.info(f"Creating ticket for {meta.get("parent_event_ksuid")}, {organisation_slug} into {TABLE_NAME}")    
+
+    current_time = datetime.now(timezone.utc).isoformat(timespec='milliseconds').replace('+00:00', 'Z')
+    logger.info(f"Current Time: {current_time}")
+
+    # What information is in the meta?
+    # Where am I getting all of the ticket info from?
+    # Do I need to get anything from Stripe to get the info?
+    # The meta should probably have all that is needed:
+    #     - parent_event_ksuid
+    #     - customer_email
+    #     - name_on_ticket
+    #     - name
+    #     - includes
+    # How will I generate the child items from this info?
+    # Do I need to jsut use the line_items from the Stripe checkout session stored in the meta?
+
+    ticket_model = TicketModel.model_validate({
+        "ksuid": str(KsuidMs()),
+        "organisation": organisation_slug,
+        "created_at": current_time,
+        "updated_at": current_time
+        })    
 
 def lambda_handler(event, context):
-    try:
-        logger.info("Received event: %s", json.dumps(event, indent=2, cls=DecimalEncoder))
-        parsed_event = parse_event(event)
-        http_method  = event['requestContext']["http"]["method"]
+    logger.info("Received EventBridge event: %s", json.dumps(event, indent=2, cls=DecimalEncoder))
 
-        organisationSlug = event.get("pathParameters", {}).get("organisation")
-        is_public        = event.get("rawPath", "").startswith("/public")
-        actor            = event.get("requestContext", {}).get("accountId", "unknown")
+    type = event.get("detail-type", {})
+    organisationSlug = event.get("detail", {}).get("organisation", {})
 
-        # POST
-        if http_method == "POST":
-            return make_response(405, {"message": "Method not implemented."})
-        # PUT 
-        if http_method == "PUT":
-            return make_response(405, {"message": "Method not implemented."})
-        # GET 
-        elif http_method == "GET":
-            response_cls = TicketsResponse # The model response class defined in pydantic
+    if type == "checkout.completed":
+        logger.info("Handling checkout.session.expired event.")
+        validated_request = EventBridgeEvent(**event)
 
-            if not organisationSlug:
-                return make_response(404, {"message": "Missing organisation in request"})            
-            
-            result = get(organisationSlug)
-            response = response_cls(tickets=result)
-            return make_response(200, response.model_dump(mode="json", exclude_none=True))     
-
-    except ValueError as e:
-        logger.error("Validation error: %s", str(e))
-        logger.error(traceback.format_exc())
-        return make_response(400,{"message": "Validation error.", "error": str(e)})
-    except Exception as e:
-        logger.error("Unexpected error: %s", str(e))
-        logger.error(traceback.format_exc())
-        return make_response(500, {"message": "Internal server error.", "error": str(e)})
-    
+        return create_ticket(validated_request, organisationSlug)
+    else:
+        logger.warning(f"Received event type I can not currently process: {type}")
+        return make_response(400, {"message": f"Unhandled event type: {type}"})

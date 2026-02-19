90e0827dfc3b0c9ae8570f53d9fc83185e6f8d0e
Connor Monaghan <connor1monaghan@gmail.com>
2026-02-19 00:34:34 +0000

    🎉 Scaffold Tickets lambda

---
+++ functions/functions/tickets/lambda_tickets.py
@@ -0,0 +1,82 @@
+## python libraries
+import os
+import json
+import boto3 # not a python library but is included in lambda without need to install it
+import logging
+import traceback
+import sys
+from datetime import datetime, timezone
+
+## installed packages
+from pydantic import AfterValidator, ValidationError # layer: pydantic
+#from boto3.dynamodb.conditions import Key
+#from ksuid import KsuidMs # layer: utils
+
+## custom scripts
+sys.path.append(os.path.dirname(__file__))
+from _shared.parser import parse_event
+from _shared.DecimalEncoder import DecimalEncoder
+from _shared.helpers import make_response
+# from _pydantic.models.tickets_models import TicketsObject, TicketsResponse
+#from _pydantic.models.models_extended import TicketsModel
+#from _pydantic.EventBridge import triggerEBEvent, trigger_eventbridge_event, EventType, Action # pydantic layer
+#from _pydantic.dynamodb import VersionConflictError # pydantic layer
+#from _pydantic.dynamodb import batch_write, transact_upsert, VersionConflictError # pydantic layer
+
+## logger setup
+logger = logging.getLogger()
+logger.setLevel("INFO")
+
+## aws resources an clients
+#db = boto3.resource("dynamodb")
+
+## ENV variables
+# will throw an error if the env variable does not exist
+STAGE_NAME = os.environ.get('STAGE_NAME') or (_ for _ in ()).throw(KeyError("Environment variable 'STAGE_NAME' not found"))
+
+## write here the code which is called from the handler
+def get(organisationSlug: str, public: bool = False, actor: str = "unknown") -> TicketsObject:
+    '''
+    You expect me to return an instance of TicketsObject.
+    '''
+    # TODO: implement
+    return make_response(201, {
+            "message": "It's a work in progress...",
+        })
+
+def lambda_handler(event, context):
+    try:
+        logger.info("Received event: %s", json.dumps(event, indent=2, cls=DecimalEncoder))
+        parsed_event = parse_event(event)
+        http_method  = event['requestContext']["http"]["method"]
+
+        organisationSlug = event.get("pathParameters", {}).get("organisation")
+        is_public        = event.get("rawPath", "").startswith("/public")
+        actor            = event.get("requestContext", {}).get("accountId", "unknown")
+
+        # POST
+        if http_method == "POST":
+            return make_response(405, {"message": "Method not implemented."})
+        # PUT 
+        if http_method == "PUT":
+            return make_response(405, {"message": "Method not implemented."})
+        # GET 
+        elif http_method == "GET":
+            response_cls = TicketsResponse # The model response class defined in pydantic
+
+            if not organisationSlug:
+                return make_response(404, {"message": "Missing organisation in request"})            
+            
+            result = get(organisationSlug)
+            response = response_cls(tickets=result)
+            return make_response(200, response.model_dump(mode="json", exclude_none=True))     
+
+    except ValueError as e:
+        logger.error("Validation error: %s", str(e))
+        logger.error(traceback.format_exc())
+        return make_response(400,{"message": "Validation error.", "error": str(e)})
+    except Exception as e:
+        logger.error("Unexpected error: %s", str(e))
+        logger.error(traceback.format_exc())
+        return make_response(500, {"message": "Internal server error.", "error": str(e)})
+    

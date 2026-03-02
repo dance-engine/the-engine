b5b55e8eeaf0d013c45182691ba6cc4d97a6760b
Adam Bardsley <adam.bardsley@gmail.com>
2026-03-02 14:50:23 +0000

    📝 Updates artist biographies for consistency
    Refines HTML entity usage in artist bios to ensure proper display and readability.
    
    No functional changes were made; only formatting improvements were applied across multiple entries.

--- apps/clients/solo/app/artists/artists-data.ts
+++ apps/clients/solo/app/artists/artists-data.ts
@@ -10,8 +10,8 @@ export const artists: Record<string, ArtistType> = {
   robyn: {
     name: "Robyn Body Wisdom",
     bio: [
-      "Robyn teaches Joyful Movement which is inspired by yoga, but it's a mix of lots of different things: Tai Chi, dance, martial arts, kids yoga, improvisation, lots of general silliness and fun. ",
-      "Joyful movement works on movements to improve coordination, balance and body awareness and control, experimenting with what our body can do. It's a great way to energise, relax, release and prepare the body for other types of movement. There’s no right or wrong, there’s no being ‘good at it’ or ‘bad at it and it’s not about what it looks like. ",
+      "Robyn teaches Joyful Movement which is inspired by yoga, but it&apos;s a mix of lots of different things: Tai Chi, dance, martial arts, kids yoga, improvisation, lots of general silliness and fun.",
+      "Joyful movement works on movements to improve coordination, balance and body awareness and control, experimenting with what our body can do. It&#39;s a great way to energise, relax, release and prepare the body for other types of movement. There&#39;s no right or wrong, there&#39;s no being &#39;good at it&#39; or &#39;bad at it and it&#39;s not about what it looks like. ",
       "Robyn will be the first class of the day at our event to help get dances out of their heads and into their bodies and experience the joy movement can bring us, in a safe and non-judgmental space."
     ],
     image: "/artists/robyn.jpg",
@@ -22,7 +22,7 @@ export const artists: Record<string, ArtistType> = {
     name: "Maribel Torres",
     bio: [
       "Maribel is a passionate dancer and instructor with over 10 years of experience in the Kizomba and Urban Kiz scene. Originally from Spain and based in the UK, where she founded her own brand MCR UKIZ and teaches nationally and internationally. ",
-      "She took part in the Olympiads of Kizomba in London and was later recognised as finalist Best Female Teacher of the Year in 2019. Also a judge in UK competitions like Battle Kizz, Maribel has proudly trained  winning dancers. ",
+      "She took part in the Olympiads of Kizomba in London and was later recognised as finalist Best Female Teacher of the Year in 2019. Also a judge in UK competitions like Battle Kizz, Maribel has proudly trained winning dancers.",
       "Known for her sharp footwork, body movement, and bold energy, you are not ready for this charismatic mamacita!🔥"  
     ],
     image: "/artists/maribel.jpg",
@@ -32,7 +32,7 @@ export const artists: Record<string, ArtistType> = {
   betts: {
     name: "Betts Corrigan",
     bio: [
-      "Betts Corrigan has been dancing since the age of 5, training in ballet, contemporary, and jazz before attending full-time dance school from 16–18.",
+      "Betts Corrigan has been dancing since the age of 5, training in ballet, contemporary, and jazz before attending full-time dance school from 16-18.",
       "After moving to Latin America to immerse herself in salsa, she returned to Manchester and discovered Brazilian Zouk and Lambada, training and performing around the world. ",
       "Since 2022, she has been teaching Brazilian Zouk in Manchester and internationally, qualifying with MAC in 2023 and completing Lambada Mastery in 2024. In 2025, Betts launched a dedicated follower performance group. ",
       "She now runs regular Brazilian Zouk courses in Manchester"
@@ -44,7 +44,7 @@ export const artists: Record<string, ArtistType> = {
   jeyda: {
     name: "Jeyda Salih",
     bio: [
-      "Jeyda started training in ballet, hip-hop, commercial and contemporary on-and-off since she was 3 years old. She obtained a dance scholarship at Pineapple Studios and a Bachelor's Degree in Dance Studies in London in 2017, and now works as a professional dancer and special action artist for TV, film and events.",
+      "Jeyda started training in ballet, hip-hop, commercial and contemporary on-and-off since she was 3 years old. She obtained a dance scholarship at Pineapple Studios and a Bachelor&apos;s Degree in Dance Studies in London in 2017, and now works as a professional dancer and special action artist for TV, film and events.",
       "She fell in love with Urban Kiz & Tarraxo in 2021 and has been teaching with her partner Lucas in Manchester for 3 years. They have taught workshops bootcamps all over the UK and abroad, and frequently attend international festivals as guests. In 2023, they became the first couple in the UK to be certified in Tarraxo by Gwany and Lilliana, and were titled 1st Place Urban Kiz UK Champions in the UK Open Competition.",
       "Jeyda loves to share knowledge, bring good vibes, and value dancers who are up for a challenge - we are super excited to have her bring the Urban Kiz flare to Liverpool this spring."
     ],
@@ -58,7 +58,7 @@ export const artists: Record<string, ArtistType> = {
       "Sasha Phillips started her dance journey from the age of 17. She has been teaching Salsa internationally since 2017. Professionally trained in ballet, contemporary and Jazz at the Conservatoire in London, Sasha developed her love for salsa and Latin music around the age of 20.",
       "Sasha began her salsa training in a dance school in London in which she learned and studied Cali salsa from Colombia. She spent many years developing the knowledge and understanding of that style, which led to her doing many shows in a team but also as a couple.",
       "She started to travel to international congresses where she discovered other styles of salsa such as LA, on2, afro-cuban. She has trained with several top artists in this industry such as Bersy Cortez, Fernando Sosa, Tania, Anita, Angelo Rito and Maykelfonts.",
-      "There are two aspects to Sasha's style. One of which includes mastering an understanding the use of body movement within salsa, also the expression of movement through the music, her body and partner work. Sasha has taken part in many internal shows and competitions. Her ladies team came 1st in Miami salsa summit 2023. Also, Sasha came first in the salsanama improvisations couple.",
+      "There are two aspects to Sasha&apos;s style. One of which includes mastering an understanding the use of body movement within salsa, also the expression of movement through the music, her body and partner work. Sasha has taken part in many internal shows and competitions. Her ladies team came 1st in Miami salsa summit 2023. Also, Sasha came first in the salsanama improvisations couple.",
       "Sasha is the director and choreographer of her own team called salsa furia. She currently has around 4 team around the UK. She travels nationally and international with her team to perform and compete in different salsa events and competitions. We are so exciting to have the queen of salsa herself teaching at our event with her Liverpool Debut."
     ],
     image: "/artists/sasha.jpg",
@@ -80,7 +80,7 @@ export const artists: Record<string, ArtistType> = {
     name: "Ola",
     bio: [
       "Born in Poland, Ola has a background in figure skating which she did from 7 years of age as well as some experience in the dance world with ballroom and ballet.",
-      "In 2015 she started her journey with Latin dance beginning with Cuban salsa and then bachata in Spain in 2017. She's expanded her dancing with more dance styles such as heels and reggaeton.",
+      "In 2015 she started her journey with Latin dance beginning with Cuban salsa and then bachata in Spain in 2017. She&apos;s expanded her dancing with more dance styles such as heels and reggaeton.",
       "Bringing a super feminine energy to POW with Bachata heels styling class expect a playful experience with great energy and empowering vibes."
     ],
     image: "/artists/ola.jpg",
@@ -91,7 +91,7 @@ export const artists: Record<string, ArtistType> = {
     name: "Lide Viñas",
     bio: [
       "Originally from the Basque Country (Spain) and based in Liverpool, Lide is a curious and versatile dancer who loves to learn and to share her joy for movement. Over the years she has extensively studied belly dance, ballet, and flamenco, and has recently become involved in the local SBK community.",
-      "Lide will be teaching belly dance will be an open class for all and will be a fun and accessible way for dancers to get a glimpse of this art form - please note you do not need to expose your belly for this class if you don't want to, just wear whatever you feel comfortable dancing in, and feel free to bring a scarf to tie around your hips!"
+      "Lide will be teaching belly dance will be an open class for all and will be a fun and accessible way for dancers to get a glimpse of this art form - please note you do not need to expose your belly for this class if you don&apos;t want to, just wear whatever you feel comfortable dancing in, and feel free to bring a scarf to tie around your hips!"
     ],
     image: "/artists/lide.jpg",
     handle: "@lydiavines",
@@ -100,10 +100,10 @@ export const artists: Record<string, ArtistType> = {
   salk: {
     name: "Sal K Green",
     bio: [
-      "Sally has been dancing for over 22 years with a background in Ballet, Modern, Tap, Disco, Latin American and Ballroom. She qualified with a degree in Teaching in 2005 and moved to London in 2007. In addition to Education, she has several years' experience Teaching and Coaching sport, is also a qualified Assessor and now First Aid Trainer.",
+      "Sally has been dancing for over 22 years with a background in Ballet, Modern, Tap, Disco, Latin American and Ballroom. She qualified with a degree in Teaching in 2005 and moved to London in 2007. In addition to Education, she has several years&apos; experience Teaching and Coaching sport, is also a qualified Assessor and now First Aid Trainer.",
       "Her background and love for Teaching drives her passion to develop others, help people learn and share in a united love that is music, connection & dance. Sal teaches the foundations for Beginners/Improvers, Leading, Following and loves bringing her Intermediate Musicality & Connection class to events and festivals.",
-      "She is often supporting other Kizomba events and fellow artists because she very much believes 'When people support each other, incredible things can happen', so let's root for each other and watch the industry grow, for alone we can do so little, but together we can achieve so much.",
-      "You will find Sal in various locations teaching at different events, always with a smile on her face, as well as partying on the dance floor for hours. She will be bringing the kizomba vibes to POW with multiple classes and then bringing the party to the SBK social as our Dj, 'DJ LallyK'."
+      "She is often supporting other Kizomba events and fellow artists because she very much believes &#39;When people support each other, incredible things can happen&#39;, so let&#39;s root for each other and watch the industry grow, for alone we can do so little, but together we can achieve so much.",
+      "You will find Sal in various locations teaching at different events, always with a smile on her face, as well as partying on the dance floor for hours. She will be bringing the kizomba vibes to POW with multiple classes and then bringing the party to the SBK social as our Dj, &apos;DJ LallyK&apos;."
     ],
     image: "/artists/salk.jpg",
     handle: "@sal_k_green",
@@ -124,7 +124,7 @@ export const artists: Record<string, ArtistType> = {
       "The commercial heels queen of Northwest to our event, the one and only Paige Goulding!",
       "After graduating with a first class BA Hons in Contemporary dance, Paige has established herself in the commercial industry through owning PNGDance and the professional heels company; The Stilouettes UK. Teaching, performing and choreographing across the UK and internationally.",
       "Professional credits include: Adidas, BBC, T-Pain, Nike, Tinchy Stryder, Chipmunk, Maxwell D, Dream Dolls Ibiza, Doll Beauty, Sky, Miley Cyrus and many more.",
-      "We are so excited to have Paige teaching a commercial heels class at our event this April. Don't miss out on your chance to learn with the best!"
+      "We are so excited to have Paige teaching a commercial heels class at our event this April. Don&apos;t miss out on your chance to learn with the best!"
     ],
     image: "/artists/paigeg.jpg",
     handle: "@paigenicole_goulding",
@@ -136,7 +136,7 @@ export const artists: Record<string, ArtistType> = {
       "Originally from Mexico, Fernanda began her path through dance at the age of 12 in jazz and Mexican folk dance schools. At 17, she began to dance in choreographic groups of musical comedy, modern dance and hip hop where at the same time she had the opportunity to dance with various Mexican artists for TV.",
       "This is when Fernanda began her career in crossbody salsa participating in shows and competitions nationwide. At the age of 19, she began a degree in contemporary dance at the Morelos Center for the Arts, where she was able to learn various dance techniques including ballet, afro-cuban, modern, and folklore, among many others.",
       "Later on, she moved to Playa del Carmen, Cancun, Mexico, where she began working as a professional dancer and continued her career as a performer with live bands dancing in different dance styles.",
-      "Fernanda currently teaches Salsa & Bachata and continues to share her passion for dance and all of the Latin American culture here in the UK. As one of Liverpool's SBK teachers, we are super excited to have her at our event next spring."
+      "Fernanda currently teaches Salsa & Bachata and continues to share her passion for dance and all of the Latin American culture here in the UK. As one of Liverpool&apos;s SBK teachers, we are super excited to have her at our event next spring."
     ],
     image: "/artists/fernanda.jpg",
     handle: "@fernandanavi",
@@ -145,7 +145,7 @@ export const artists: Record<string, ArtistType> = {
   sugagill: {
     name: "Sugagill",
     bio: [
-      "Gill has been dancing Latin styles professionally since 2007. Starting off in Melbourne Australia in a company called 'Katumba Latin Entertainment,' she performed weekly at restaurants and high end corporate events for 4 years. She has continued to dance Samba and Salsa on returning to the UK.",
+      "Gill has been dancing Latin styles professionally since 2007. Starting off in Melbourne Australia in a company called &#39;Katumba Latin Entertainment,&#39; she performed weekly at restaurants and high end corporate events for 4 years. She has continued to dance Samba and Salsa on returning to the UK.",
       "Working for Viva Brasil Samba shows from 2011 until 2020, she performed regularly at Alma De Cuba and at many events across the UK and afar including Cannonball Run (Ireland), Salsa Northwest UK Congress, Blue Peter, Strictly Come Dancing, ITV Fever Pitch during the World Cup in Brasil, and Havana De Primera music video in Cuba.",
       "In 2012 she won the title of Brazilica Carnival Queen and then competed with Mangueira at the Rio Carnival in 2016, the year in which they won. In 2019 she performed with Nile Rogers and CHIC at the Liverpool Music Festival which she says was the highlight of her career.",
       "We are super excited to have Gill as a part of our event bringing the Samba vibes on her home turf in Liverpool!"
@@ -172,7 +172,7 @@ export const artists: Record<string, ArtistType> = {
       "Laura started her dancing career at a young age in musical theatre. This is where her passion to perform came from. She explored many dance genres in her earlier years, including contemporary, Irish, jazz, and commercial.",
       "She had her first ever salsa lesson when she was just 14 years old and quickly became hooked. Now, in her mid 30s, she enjoys unique fusion blends with salsa, inspired by her love of hip hop music and dance.",
       "Laura is grateful of all the opportunities her dance career has given her, including teaching at countless festivals, choreographing for music videos, activism campaigns and even a guest choreographer at New York fashion week.",
-      "At POW she will be treating us to a commercial hip-hop fusion class and can't wait to share her passion and creativity with the women of the Northwest!"
+      "At POW she will be treating us to a commercial hip-hop fusion class and can&apos;t wait to share her passion and creativity with the women of the Northwest!"
     ],
     image: "/artists/laura.jpg",
     handle: "@lauramarveldance",

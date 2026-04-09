17ef8e757e1df90b3465bc69da9fb382206d86f5
Adam Bardsley <adam.bardsley@gmail.com>
2026-04-09 17:25:23 +0100

    Enhances the UI of the artists' list by adding a `justify-center` class to the wrapping div and improving the artist card layout with background images, ensuring a more polished presentation.

--- apps/clients/solo/app/artists/page.tsx
+++ apps/clients/solo/app/artists/page.tsx
@@ -18,7 +18,7 @@ const ArtistsListPage = async () => {
     `;
   const h = await headers();
   const orgSlug = h.get('x-site-org') || 'default-org';
-  // const theme = h.get('x-site-theme') || 'default';
+  const theme = h.get('x-site-theme') || 'default';
   const ORGS_API_URL = `${process.env.NEXT_PUBLIC_DANCE_ENGINE_API}/public/organisations`
   const orgs_res = await fetch(ORGS_API_URL, { cache: 'force-cache', next: { revalidate: 120, tags: [format(new Date(), 'yyyy-MM-ddTHH:mm:ss.SSSxxx')] } });
   const orgs_data = await orgs_res.json()
@@ -36,10 +36,14 @@ const ArtistsListPage = async () => {
           <div className={`max-w-4xl w-4xl px-4 lg:px-0 mb-12 text-white text-xl `}> 
               <h1 className='text-4xl my-8'>Artists</h1>
               <p className='mb-8 text-lg leading-relaxed'>We&apos;re thrilled to bring together an incredible lineup of world-class dancers and choreographers from across the UK and beyond. Each artist brings their unique style, passion, and expertise to create an unforgettable experience. From salsa and bachata to contemporary movement and samba, our artists are dedicated to inspiring, educating, and connecting dancers of all levels. Click on any artist below to learn more about their journey and what they&apos;ll be sharing at our event.</p>
-              <div className='sm:flex flex-wrap gap-3'>
+              <div className='sm:flex flex-wrap gap-3 justify-center'>
                  {Object.keys(artists).sort().map((artistKey) => (
-                <div key={artistKey} className='mb-2'>
-                  <Link className="text-2xl bg-[var(--highlight-color)] px-3 py-1 hover:underline block rounded" href={`/artists/${artistKey}`}>{artists[artistKey].name}</Link>
+                <div key={artistKey} className='mb-2 rounded w-72'>
+                  <div id="hero" className={`w-full min-h-[200px] rounded-t-md text-white flex items-stretch justify-center bg-no-repeat \
+                  ${theme == 'latin-soul' ?  "bg-cover bg-center": "bg-cover bg-top sm:bg-center md:bg-top sm:bg-size-[1400px_auto] md:bg-cover"}`} 
+                  style={{ backgroundImage: `url(${artists[artistKey]?.image || '/artists/default.jpg'})` }}
+                ></div>
+                  <Link className="text-2xl bg-[var(--highlight-color)] text-shadow-neutral-900 text-shadow-2xs px-3 py-1 hover:underline block rounded-b-md w-full " href={`/artists/${artistKey}`}>{artists[artistKey].name}</Link>
                 </div>
               ) )}
               </div>
